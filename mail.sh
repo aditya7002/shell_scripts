8a205d6f536b2f26df4de1094a1ea22fd10562f2
Aditya Datta <aditya7002@gmail.com>
2019-04-29 09:33:50 -0400

    Shell script demonstrating mail sending

---
+++ mail.sh
@@ -0,0 +1,5 @@
+# WAP to demonstrate mail sending using shell scripting
+
+# For using mail command, we need to install mailx and postfix
+
+mail -s "Hello Aditya" aditya7002@gmail.com <<< 'I am not happy'
